a18fe2fec00c3fb590872b1576a9c8d4839b9405
Octopus142 <blackfairyforma726@gmail.com>
2024-10-08 17:50:34 +0800

    Update fermat1.ts

--- tensor/fermat/fermat1.ts
+++ tensor/fermat/fermat1.ts
@@ -5,6 +5,6 @@ import './tensor/outintensor'
 import './tensor/antiinouttensor'
 import './tensor/antioutinttensor'
 void function fermat1(){
-   void ['innertensor' +'outertensor'+'antiinouttensor' === 'outintensor' +  'outertensor' +'antioutintensor']
+   void ['innertensor' +'outintensor'+'antioutintensor' === 'outertensor' +  'outintensor' +'antioutintensor']
    return fermat1
 }
