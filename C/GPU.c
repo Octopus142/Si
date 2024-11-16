4ab1bdded4b45067636606c9d234d83438381338
Octopus142 <blackfairyforma726@gmail.com>
2024-11-17 00:59:09 +0800

    Update GPU.c

--- C/GPU.c
+++ C/GPU.c
@@ -1,9 +1,9 @@
 #include "CPU.c"
 void function(){
-const fx1 = 'x^7' + 'w1' * 'x^3' + 'w2' * 'x^2' + 'w3' * 'x' ;
-const fx2 = 'x^7' + "w1' * x^3" - "w2' * x^2" + "w3' * 'x" ;
-const fx3 = 'y^7' + 'w1' * 'y^3' + 'w2' * 'y^2' + 'w3' * 'y' ;
-const fx4 = 'y^7' + "w1' * 'y^3" - "w2' * y^2" + "w3' * y" ;
-const fx5 = 'z^7' + 'w1' * 'z^3' + 'w2' * 'z^2' + 'w3' * 'z' ;
-const fx6 = 'z^7' + "w1' * 'z^3" - "w2' * z^2" + "w3' * z" ;
+const fx7 = 'x^7' + 'w1' * 'x^3' + 'w2' * 'x^2' + 'w3' * 'x' ;
+const fx8 = 'x^7' + "w1' * x^3" - "w2' * x^2" + "w3' * 'x" ;
+const fx9 = 'y^7' + 'w1' * 'y^3' + 'w2' * 'y^2' + 'w3' * 'y' ;
+const fx10 = 'y^7' + "w1' * 'y^3" - "w2' * y^2" + "w3' * y" ;
+const fx11 = 'z^7' + 'w1' * 'z^3' + 'w2' * 'z^2' + 'w3' * 'z' ;
+const fx12 = 'z^7' + "w1' * 'z^3" - "w2' * z^2" + "w3' * z" ;
 }
