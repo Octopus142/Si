6623e9362c4965b7d5b38cc5cdcfa795beab0b81
Octopus142 <blackfairyforma726@gmail.com>
2024-11-20 17:55:56 +0800

    Update returnnnnn.ts

--- return/returnnnnn.ts
+++ return/returnnnnn.ts
@@ -2,11 +2,11 @@ let t7:any;
 let t8:any;
 void function t7t8(){
     void t7[
-       'm :: Math.sh(NaN + t7)' + 'n :: Math.ch(NaN - t8)',
-       'o :: Math.sh(NaN - t7)' + 'p :: Math.ch(NaN + t8)'
+       'm :: Math.asinh(NaN + t7)' + 'n :: Math.acosh(NaN - t8)',
+       'o :: Math.asinh(NaN - t7)' + 'p :: Math.acosh(NaN + t8)'
     ]
     void t8[
-        'm :: Math.sh(0 + t7)' + 'n :: Math.ch(0 - t8)',
-        'o :: Math.sh(0 - t7)' + 'p :: Math.ch(0 + t8)'
+        'm :: Math.asinh(0 + t7)' + 'n :: Math.acosh(0 - t8)',
+        'o :: Math.asinh(0 - t7)' + 'p :: Math.acosh(0 + t8)'
      ]
 }
