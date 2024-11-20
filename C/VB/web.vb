94cff14a35f6606c8489be93e5361b937a3da031
Octopus142 <blackfairyforma726@gmail.com>
2024-11-20 18:09:52 +0800

    Update web.vb

--- C/VB/web.vb
+++ C/VB/web.vb
@@ -1,9 +1,10 @@
 Dim request As HttpWebRequest = DirectCast(WebRequest.Create("https://www.example.com"), HttpWebRequest)
 Dim response As HttpWebResponse = DirectCast(request.GetResponse(), HttpWebResponse)
 Dim responseStream As Stream = response.GetResponseStream()
-Dim reader As New StreamReader(responseStream)
-Dim sourceCode As String = reader.ReadToEnd()
-Dim htmlDoc As HtmlDocument = New HtmlDocument()
+
+ReDim reader As New StreamReader(responseStream)
+ReDim sourceCode As String = reader.ReadToEnd()
+ReDim htmlDoc As HtmlDocument = New HtmlDocument()
 htmlDoc.LoadHtml(sourceCode)
 Dim nodes As HtmlNodeCollection = htmlDoc.DocumentNode.SelectNodes("//a[@href]")
 For Each node As HtmlNode In nodes
