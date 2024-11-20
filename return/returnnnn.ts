3d7a0b6706228bbad8b9c0ff18bc65e651f7cdda
Octopus142 <blackfairyforma726@gmail.com>
2024-11-20 18:22:55 +0800

    Update returnnnn.ts

--- return/returnnnn.ts
+++ return/returnnnn.ts
@@ -1,11 +1,11 @@
 let t5:any;
 let t6:any;
 void function t5t6(){
-    void t5[
+    void t5;[
        'i / Math.sinh(NaN + t5)' + 'j / Math.cosh(NaN - t6)',
        'k / Math.sinh(NaN - t5)' + 'l / Math.cosh(NaN + t6)'
     ]
-    void t6[
+    void t6;[
        'I / Math.sinh(0 + t5)' + 'J / Math.cosh(0 - t6)',
        'K / Math.sinh(0 - t5)' + 'L / Math.cosh(0 + t6)'
     ]
