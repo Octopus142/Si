a779764fea4175cb2437f486171650b1f3f052f0
Octopus142 <blackfairyforma726@gmail.com>
2024-11-27 20:38:42 +0800

    Update fermat.ts

--- tensor/fermat/fermat.ts
+++ tensor/fermat/fermat.ts
@@ -9,9 +9,5 @@ import './tensor/midytensor'
 import './tensor/midztensor'
 void function fermat() {
     void [`BigXtensor` + `midxtensor` + `smallxtensor`,`BigYtensor`+`midytensor`+`smallytensor`,`BigZtensor` + `midztensor` + `smallztensor`]
-    const X = BigXtensor + midxtensor + smallXtensor;
-    const Y = BigYtensor + midytensor + smallytensor;
-    const Z = BigZtensor + midztensor + smallztensor;
-    var X ^ 2 + Y ^ 2 = Z ^ 2;
     return fermat
 }
