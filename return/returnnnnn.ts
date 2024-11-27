88eb618b2592036b7078c9b3c6b291ed8ef7aeca
Octopus142 <blackfairyforma726@gmail.com>
2024-11-27 20:41:01 +0800

    Update returnnnnn.ts

--- return/returnnnnn.ts
+++ return/returnnnnn.ts
@@ -6,7 +6,7 @@ void function t7t8(){
         'o :: Math.max(NaN - t7)' + 'p :: Math.min(NaN + t8)'
     ]
     void t8;[
-        'm :: Math.max(0 + t7)' + 'n :: Math.min(0 - t8)',
-        'o :: Math.max(0 - t7)' + 'p :: Math.min(0 + t8)'
+        'M :: Math.max(0 + t7)' + 'N :: Math.min(0 - t8)',
+        'O :: Math.max(0 - t7)' + 'P :: Math.min(0 + t8)'
      ]
 }
