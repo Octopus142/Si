329a7f4d27a73d98347a3afda6cfa27ac5615a33
Octopus142 <blackfairyforma726@gmail.com>
2024-11-28 12:46:37 +0800

    Update returnnn.ts

--- return/returnnn.ts
+++ return/returnnn.ts
@@ -1,6 +1,6 @@
 let t3:any;
 let t4:any;
-void function t3t4(){
+void function returnnn(){
     void t3;[
        'e % Math.asin(NaN + t3)' + 'f % Math.acos(NaN - t4)',
        'g % Math.asin(NaN - t3)' + 'h % Math.acos(NaN + t4)'
