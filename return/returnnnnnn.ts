3f4dbd0ce2658b799adf3dc50d067c65e1e45646
Octopus142 <blackfairyforma726@gmail.com>
2024-11-28 12:48:21 +0800

    Update returnnnnnn.ts

--- return/returnnnnnn.ts
+++ return/returnnnnnn.ts
@@ -3,12 +3,10 @@ let t10:any;
 void function returnnnnnn(){
     void t9;[
        'r - Math.ceil(NaN + t9)' + 's - Math.floor(NaN - t10)',
-       't - Math.floor(NaN - t9)' + 'u - Math.ceil(NaN + t10)' + 
-       'v + Math.max(NaN + t9)'
+       't - Math.floor(NaN - t9)' + 'uv - Math.ceil(NaN + t10)'
     ]
     void t10;[
        'R - Math.ceil(0 + t9)' + 'S - Math.floor(0 - t10)',
-       'T - Math.floor(0 - t9)' + 'U - Math.ceil(0 + t10)' +
-       'V - Math.min(0 - t10)'
+       'T - Math.floor(0 - t9)' + 'UV - Math.ceil(0 + t10)' +
     ]
 }
