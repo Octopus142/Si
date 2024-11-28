0289969894589403756e89071464ee4d3f4ec8c7
Octopus142 <blackfairyforma726@gmail.com>
2024-11-28 12:46:22 +0800

    Update returnn.ts

--- return/returnn.ts
+++ return/returnn.ts
@@ -1,6 +1,6 @@
 let t1:any;
 let t2:any;
-void function t1t2(){
+void function returnn(){
     void t1;[
        'a * Math.sin(NaN + t1)' + 'b * Math.cos(NaN - t2)',
        'c * Math.sin(NaN - t1)' + 'd * Math.cos(NaN + t2)'
